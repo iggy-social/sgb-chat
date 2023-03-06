9478cd03974cf0841a479ef2827bfcd3a199cebf
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2023-03-06 20:07:45 +0100

    NFT and minter addresses added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -52,8 +52,8 @@ export default defineNuxtConfig({
     public: {
       blockExplorerBaseUrl: "https://songbird-explorer.flare.network",
       favicon: "/img/favicon.png",
-      iggyPostAddress: "0x9f48c192561f3A6f0efeeE5Fce00Fd9788675eF8",
-      iggyPostMinterAddress: "0xD48e9b2D25CEe123be1d01c09645A0a355174db0",
+      iggyPostAddress: "0x99Dbf11aCd46baFBCE82506FaeB4F13E6Ea1726A",
+      iggyPostMinterAddress: "0xabf9960132818049340253C3Ca0551F92Db856d7",
       orbisContext: "kjzl6cwe1jw14a064memywn3fhuks5w3m2xfole8vrj202suz4xq8o9yfhso6hf", // production context
       orbisTest: true, // if true, test context will be used instead of the production one
       orbisTestContext: "kjzl6cwe1jw145tfqv2eqv8tiz6puo27meyz4smz40atppuc13tulqca87k35z2", // test context
