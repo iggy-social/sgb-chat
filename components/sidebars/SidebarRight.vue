df63c671e29d908f9c5b8c689cccd722a311767c
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2023-03-10 11:23:52 +0100

    newsletter widget link updated

--- components/sidebars/SidebarRight.vue
+++ components/sidebars/SidebarRight.vue
@@ -8,7 +8,7 @@
       <div class="card m-2 bg-light">
         <div class="card-header bg-light">SGB Chat Newsletter</div>
         <div class="card-body sidebar-card-body">
-          <a class="btn btn-outline-primary mt-2 mb-2" target="_blank" href="https://zcmp.eu/T6H">Join our newsletter!</a>
+          <a class="btn btn-outline-primary mt-2 mb-2" target="_blank" href="https://zcmp.eu/Onz">Join our newsletter!</a>
         </div>
       </div>
 
