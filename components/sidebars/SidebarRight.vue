a5284e6c1c73af5a1ece03036001f6aee7024a48
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2023-07-20 16:04:33 +0200

    SGB Chat Playlist

--- components/sidebars/SidebarRight.vue
+++ components/sidebars/SidebarRight.vue
@@ -11,6 +11,13 @@
 
       <!-- Random minted post(s) -->
       <MintedPostsWidget @closeRightSidebar="closeRightSidebar" />
+
+      <div class="card m-2 bg-light">
+        <div class="card-header bg-light">SGB Chat Playlist</div>
+        <div class="card-body sidebar-card-body">
+          <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DX3b9hbbPi5hD?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        </div>
+      </div>
       
     </div>
   </div>
