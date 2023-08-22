c1c6d6a64d747061032936eb34e2ca655ca32090
Nidz The Fact <106298826+nidz-the-fact@users.noreply.github.com>
2023-08-22 18:01:24 +0700

    Update swap.vue
    scene for reloading

--- pages/swap.vue
+++ pages/swap.vue
@@ -4,6 +4,10 @@
     <Meta property="og:title" :content="'Token Swap | ' + $config.projectMetadataTitle" />
   </Head>
 
+  <div v-if="isLoading" class="loading-image">
+    <img src="/img/sgb-chat-logo-2.png" class="loading-spinner" /> 
+  </div>
+
   <div class="scroll-500">
     <div class="card border">
       <div class="card-body mb-3">
@@ -30,11 +34,20 @@
 <script>
 import SimpleSwap from '~~/components/swap/SimpleSwap.vue';
 
+import { ref, onMounted } from 'vue'
+
 export default {
   name: 'Swap',
 
   components: {
     SimpleSwap,
   },
+
+  setup() {
+    const isLoading = ref(true);
+     onMounted(() => { isLoading.value = false; });
+
+     return { isLoading }
+  }
 }
 </script>
