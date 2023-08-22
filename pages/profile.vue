08cae184487c4bd0a53d2df8da3e208887f9a62a
Nidz The Fact <106298826+nidz-the-fact@users.noreply.github.com>
2023-08-22 18:00:47 +0700

    Update profile.vue
    scene for reloading

--- pages/profile.vue
+++ pages/profile.vue
@@ -10,11 +10,16 @@
     <Meta name="twitter:description" :content="'Check out this profile on ' + $config.projectName + '!'" />
   </Head>
 
+  <div v-if="isLoading" class="loading-image">
+    <img src="/img/sgb-chat-logo-2.png" class="loading-spinner" /> 
+  </div>
   <PunkProfile class="mt-1" />
 </template>
 
 <script>
 import PunkProfile from '~/components/profile/PunkProfile.vue'
+
+import { ref, onMounted } from 'vue'
 
 export default {
   name: "Profile",
@@ -24,7 +29,10 @@ export default {
   },
 
   setup() {
+    const isLoading = ref(true);
+     onMounted(() => { isLoading.value = false; });
 
+     return { isLoading }
   }
 }
 </script>
