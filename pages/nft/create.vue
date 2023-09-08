a5b5fbabedec020ebd07c0145e77d0a248589ab1
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2023-09-08 19:53:26 +0200

    nft launchpad folder structure

---
+++ pages/nft/create.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Create NFT collection</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'NftCreate'
+}
+</script>
---
+++ pages/nft/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>NFT</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nft'
+}
+</script>
