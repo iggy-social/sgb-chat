e072f330d6d4dc923067f8a17b419e04a741947c
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2023-09-13 16:34:08 +0200

    find NFT collection modal

--- pages/nft/index.vue
+++ pages/nft/index.vue
@@ -1,64 +1,98 @@
 <template>
-  <Head>
-    <Title>NFT Launchpad | {{ $config.projectMetadataTitle }}</Title>
-    <Meta property="og:title" :content="'NFT Launchpad | ' + $config.projectMetadataTitle" />
+<Head>
+  <Title>NFT Launchpad | {{ $config.projectMetadataTitle }}</Title>
+  <Meta property="og:title" :content="'NFT Launchpad | ' + $config.projectMetadataTitle" />
 
-    <Meta name="description" :content="'Check out these awesome NFT collections on ' + $config.projectName + '!'" />
+  <Meta name="description" :content="'Check out these awesome NFT collections on ' + $config.projectName + '!'" />
 
-    <Meta property="og:image" :content="$config.projectUrl+$config.previewImageNftLaunchpad" />
-    <Meta property="og:description" :content="'Check out these awesome NFT collections on ' + $config.projectName + '!'" />
+  <Meta property="og:image" :content="$config.projectUrl+$config.previewImageNftLaunchpad" />
+  <Meta property="og:description" :content="'Check out these awesome NFT collections on ' + $config.projectName + '!'" />
 
-    <Meta name="twitter:image" :content="$config.projectUrl+$config.previewImageNftLaunchpad" />
-    <Meta name="twitter:description" :content="'Check out these awesome NFT collections on ' + $config.projectName + '!'" />
-  </Head>
+  <Meta name="twitter:image" :content="$config.projectUrl+$config.previewImageNftLaunchpad" />
+  <Meta name="twitter:description" :content="'Check out these awesome NFT collections on ' + $config.projectName + '!'" />
+</Head>
 
-  <div class="card border scroll-500">
-    <div class="card-body">
-      <p class="fs-3" @click="$router.back()">
-        <i class="bi bi-arrow-left-circle cursor-pointer"></i>
-      </p>
+<div class="card border scroll-500">
+  <div class="card-body">
+    <p class="fs-3" @click="$router.back()">
+      <i class="bi bi-arrow-left-circle cursor-pointer"></i>
+    </p>
 
-      <h3 class="mb-4 mt-3">
-        NFT Launchpad
-        <NuxtLink class="btn btn-outline-primary btn-sm" to="/nft/create">
-          <i class="bi bi-plus-circle"></i> Create
-        </NuxtLink>
-      </h3>
+    <h3 class="mb-4 mt-3">
+      NFT Launchpad
+      <NuxtLink class="btn btn-outline-primary btn-sm" to="/nft/create">
+        <i class="bi bi-plus-circle"></i> Create
+      </NuxtLink>
+      <button class="btn btn-outline-primary btn-sm ms-2" data-bs-toggle="modal" data-bs-target="#searchModal">
+        <i class="bi bi-search"></i> Find
+      </button>
+    </h3>
 
-      <div class="d-flex justify-content-center mb-3" v-if="waitingData">
-        <span class="spinner-border spinner-border-lg" role="status" aria-hidden="true"></span>
-      </div>
+    <div class="d-flex justify-content-center mb-3" v-if="waitingData">
+      <span class="spinner-border spinner-border-lg" role="status" aria-hidden="true"></span>
+    </div>
 
-      <h4 class="mb-3" v-if="featuredNfts">Featured</h4>
+    <h4 class="mb-3" v-if="featuredNfts.length > 0">Featured</h4>
 
-      <div class="row" v-if="featuredNfts">
-        <NuxtLink v-for="nft in featuredNfts" :key="nft.address" class="col-md-3 text-decoration-none" :to="'/nft/collection?id=' + nft.address">
-          <div class="card border mb-3">
-            <img :src="nft.image" class="card-img-top" :alt="nft.name">
-            <div class="card-body rounded-bottom-3">
-              <p class="card-text mb-1"><strong>{{ nft.name }}</strong></p>
-              <small class="card-text">{{ formatPrice(nft.price) }} {{ $config.tokenSymbol }}</small>
-            </div>
+    <div class="row" v-if="featuredNfts.length > 0">
+      <NuxtLink v-for="nft in featuredNfts" :key="nft.address" class="col-md-3 text-decoration-none" :to="'/nft/collection?id=' + nft.address">
+        <div class="card border mb-3">
+          <img :src="nft.image" class="card-img-top" :alt="nft.name">
+          <div class="card-body rounded-bottom-3">
+            <p class="card-text mb-1"><strong>{{ nft.name }}</strong></p>
+            <small class="card-text">{{ formatPrice(nft.price) }} {{ $config.tokenSymbol }}</small>
           </div>
-        </NuxtLink>
-      </div>
+        </div>
+      </NuxtLink>
+    </div>
 
-      <h4 class="mt-3 mb-3">Latest</h4>
+    <h4 class="mt-3 mb-3" v-if="lastNfts.length > 0">Latest</h4>
 
-      <div class="row">
-        <NuxtLink v-for="nft in lastNfts" :key="nft.address" class="col-md-3 text-decoration-none" :to="'/nft/collection?id=' + nft.address">
-          <div class="card border mb-3">
-            <img :src="nft.image" class="card-img-top" :alt="nft.name">
-            <div class="card-body rounded-bottom-3">
-              <p class="card-text mb-1"><strong>{{ nft.name }}</strong></p>
-              <small class="card-text">{{ formatPrice(nft.price) }} {{ $config.tokenSymbol }}</small>
-            </div>
+    <div class="row">
+      <NuxtLink v-for="nft in lastNfts" :key="nft.address" class="col-md-3 text-decoration-none" :to="'/nft/collection?id=' + nft.address">
+        <div class="card border mb-3">
+          <img :src="nft.image" class="card-img-top" :alt="nft.name">
+          <div class="card-body rounded-bottom-3">
+            <p class="card-text mb-1"><strong>{{ nft.name }}</strong></p>
+            <small class="card-text">{{ formatPrice(nft.price) }} {{ $config.tokenSymbol }}</small>
           </div>
-        </NuxtLink>
+        </div>
+      </NuxtLink>
+    </div>
+
+  </div>
+</div>
+
+<!-- Search Modal -->
+<div class="modal fade" id="searchModal" tabindex="-1" aria-labelledby="searchModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="searchModalLabel">Find NFT collection</h1>
+        <button id="closeSearchModal" type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
 
+      <div class="modal-body">
+
+        <div class="mb-3">
+          <label for="searchInputField" class="form-label">Enter NFT collection address or unique ID:</label>
+          <input v-model="searchText" type="text" class="form-control" id="searchInputField" />
+        </div>
+
+        <p v-if="findError">Error: Collection not found...</p>
+
+      </div>
+
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button @click="findNft" type="button" class="btn btn-primary" :disabled="waitingFind">
+          <span v-if="waitingFind" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+          Find
+        </button>
+      </div>
     </div>
   </div>
+</div>
 </template>
 
 <script>
@@ -71,8 +105,11 @@ export default {
   data() {
     return {
       featuredNfts: [],
+      findError: false,
       lastNfts: [],
-      waitingData: false
+      searchText: null,
+      waitingData: false,
+      waitingFind: false
     }
   },
 
@@ -106,17 +143,63 @@ export default {
         provider
       );
 
-      // get last NFTs
-      const lNfts = await launchpadContract.getLastNftContracts(4);
-
-      await this.parseNftsArray(lNfts, this.lastNfts, provider);
-
       // get featured NFTs
       const fNfts = await launchpadContract.getFeaturedNftContracts(4);
 
       await this.parseNftsArray(fNfts, this.featuredNfts, provider);
 
+      // get last NFTs
+      const lNfts = await launchpadContract.getLastNftContracts(4);
+
+      await this.parseNftsArray(lNfts, this.lastNfts, provider);
+
       this.waitingData = false;
+    },
+
+    async findNft() {
+      this.waitingFind = true;
+      this.findError = false;
+
+      if (this.searchText) {
+        if (String(this.searchText).toLowerCase().startsWith("0x")) {
+          document.getElementById("closeSearchModal").click();
+          this.$router.push({ path: '/nft/collection/', query: { id: this.searchText } });
+          this.searchText = null;
+          return this.waitingFind = false;
+        } else {
+          // search by unique ID
+          const launchpadInterface = new ethers.utils.Interface([
+            "function getNftContractAddress(string calldata _uniqueId) external view returns(address)"
+          ]);
+
+          // fetch provider from hardcoded RPCs
+          let provider = this.$getFallbackProvider(this.$config.supportedChainId);
+
+          if (this.isActivated && this.chainId === this.$config.supportedChainId) {
+            // fetch provider from user's MetaMask
+            provider = this.signer;
+          }
+
+          const launchpadContract = new ethers.Contract(
+            this.$config.nftLaunchpadBondingAddress,
+            launchpadInterface,
+            provider
+          );
+
+          const nftAddress = await launchpadContract.getNftContractAddress(this.searchText);
+
+          if (nftAddress !== ethers.constants.AddressZero) {
+            document.getElementById("closeSearchModal").click();
+            this.$router.push({ path: '/nft/collection/', query: { id: nftAddress } });
+            this.searchText = null;
+            return this.waitingFind = false;
+          }
+        }
+
+        this.findError = true;
+
+        return this.waitingFind = false;
+      }
     },
 
     formatPrice(priceWei) {
