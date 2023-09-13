350947f0c61f660cd4287e6a2fb13a81e90c7610
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2023-09-13 14:57:09 +0200

    launchpad: featured section

--- pages/nft/index.vue
+++ pages/nft/index.vue
@@ -29,9 +29,25 @@
         <span class="spinner-border spinner-border-lg" role="status" aria-hidden="true"></span>
       </div>
 
+      <h4 class="mb-3" v-if="featuredNfts">Featured</h4>
+
+      <div class="row" v-if="featuredNfts">
+        <NuxtLink v-for="nft in featuredNfts" :key="nft.address" class="col-md-3 text-decoration-none" :to="'/nft/collection?id=' + nft.address">
+          <div class="card border mb-3">
+            <img :src="nft.image" class="card-img-top" :alt="nft.name">
+            <div class="card-body rounded-bottom-3">
+              <p class="card-text mb-1"><strong>{{ nft.name }}</strong></p>
+              <small class="card-text">{{ formatPrice(nft.price) }} {{ $config.tokenSymbol }}</small>
+            </div>
+          </div>
+        </NuxtLink>
+      </div>
+
+      <h4 class="mt-3 mb-3">Latest</h4>
+
       <div class="row">
         <NuxtLink v-for="nft in lastNfts" :key="nft.address" class="col-md-3 text-decoration-none" :to="'/nft/collection?id=' + nft.address">
-          <div class="card border">
+          <div class="card border mb-3">
             <img :src="nft.image" class="card-img-top" :alt="nft.name">
             <div class="card-body rounded-bottom-3">
               <p class="card-text mb-1"><strong>{{ nft.name }}</strong></p>
@@ -93,32 +109,12 @@ export default {
       // get last NFTs
       const lNfts = await launchpadContract.getLastNftContracts(4);
 
-      const nftInterface = new ethers.utils.Interface([
-        "function collectionPreview() public view returns (string memory)",
-        "function getMintPrice() public view returns (uint256)",
-        "function name() public view returns (string memory)"
-      ]);
+      await this.parseNftsArray(lNfts, this.lastNfts, provider);
 
-      // for loop to get NFTs data (price, name & image)
-      for (let i = 0; i < lNfts.length; i++) {
-        const nftContract = new ethers.Contract(lNfts[i], nftInterface, provider);
+      // get featured NFTs
+      const fNfts = await launchpadContract.getFeaturedNftContracts(4);
 
-        // get collection name
-        const cName = await nftContract.name();
-
-        // get price
-        const mintPriceWei = await nftContract.getMintPrice();
-
-        // get image
-        const cImage = await nftContract.collectionPreview();
-
-        this.lastNfts.push({
-          address: lNfts[i],
-          image: cImage,
-          name: cName,
-          price: mintPriceWei
-        });
-      }
+      await this.parseNftsArray(fNfts, this.featuredNfts, provider);
 
       this.waitingData = false;
     },
@@ -150,6 +146,55 @@ export default {
         return price;
       }
     },
+
+    async parseNftsArray(inputArray, outputArray, provider) {
+      const nftInterface = new ethers.utils.Interface([
+        "function collectionPreview() public view returns (string memory)",
+        "function getMintPrice() public view returns (uint256)",
+        "function name() public view returns (string memory)"
+      ]);
+
+      // for loop to get NFTs data (price, name & image)
+      for (let i = 0; i < inputArray.length; i++) {
+        const nftContract = new ethers.Contract(inputArray[i], nftInterface, provider);
+
+        // fetch collection object from session storage
+        const collectionString = sessionStorage.getItem(String(inputArray[i]).toLowerCase()+"-collection");
+        let collection;
+        
+        if (collectionString) {
+          collection = JSON.parse(collectionString);
+        }
+
+        // get collection name
+        let cName;
+
+        if (collection?.name) {
+          cName = collection.name;
+        } else {
+          cName = await nftContract.name();
+        }
+
+        // get price
+        const mintPriceWei = await nftContract.getMintPrice();
+
+        // get image
+        let cImage;
+
+        if (collection?.image) {
+          cImage = collection.image;
+        } else {
+          cImage = await nftContract.collectionPreview();
+        }
+
+        outputArray.push({
+          address: inputArray[i],
+          image: cImage,
+          name: cName,
+          price: mintPriceWei
+        });
+      }
+    }
   },
 
   setup() {
